520935f51b8427d28f6f6323e8ba8fd1ab6af5d7
Anser Yu <81740803+anseryuer@users.noreply.github.com>
2026-04-12 21:00:01 -0500

    Adjust training parameters in unlearning script

--- SD_Class-wise_unlearn_NSFW_unlearn/EUPMU/unlearning_all_classes_eu.sh
+++ SD_Class-wise_unlearn_NSFW_unlearn/EUPMU/unlearning_all_classes_eu.sh
@@ -5,10 +5,10 @@ TRAIN_ENV="${TRAIN_ENV:-eupmu-h200}"
 FID_ENV="${FID_ENV:-fid-eval}"
 
 METHOD="full"
-EPOCHS=5
+EPOCHS=3
 LR="1e-05"
 W_LR="10.0"
-ERR="0.5"
+ERR="0.3"
 ALPHA="0.01"
 PROMPTS="prompts/imagenette.csv"
 SAVE_ROOT="eval_output"
@@ -17,9 +17,11 @@ SAVE_ROOT="eval_output"
 # 100 images per prompt because generate-images.py loops ten times internally.
 NUM_SAMPLES=10
 
+# eval_output/compvis-cl-class_1-method_full-epoch_3-lr_1e-05-mtl_eu-w_lr_10.0-err_0.3-alpha_0.01
+
 source /opt/conda/etc/profile.d/conda.sh
 
-for CLASS in {1..9}; do
+for CLASS in {0..9}; do
     MODEL_NAME="compvis-cl-class_${CLASS}-method_${METHOD}-epoch_${EPOCHS}-lr_${LR}-mtl_eu-w_lr_${W_LR}-err_${ERR}"
     if [[ "${ALPHA}" != "1.0" ]]; then
         MODEL_NAME="${MODEL_NAME}-alpha_${ALPHA}"
